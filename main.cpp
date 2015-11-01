2e4de29ca03e07821b6dadbf257e05f7bde42cb6
littlekuke <littlekuke@littlekukes-MacBook-Pro.local>
2015-11-02 06:57:30 +0800

    debugging

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ int main()
    PSO pso(1024);
    float time = pso.Solve(50, 10e-6);
    std::cout<<"CPU result: "<<std::endl;
-   std::cout<<"x: "<<pso.gBest.x<<"y: "<<pso.gBest.y<<"iters: "<<pso.gBest.iters<<"time: "<<time<<std::endl;
+   std::cout<<"x: "<<pso.gBest.x<<"y: "<<pso.gBest.y<<"iters: "<<pso.iters<<"time: "<<time<<std::endl;
    
    CudaPSO cuda_pso(1024);
    float cuda_time = cuda_pso.Solve(150, 10^-6);
    std::cout<<"GPU result: "<<std::endl;
-   std::cout<<"x: "<<cuda_pso.gBest.x<<"y: "<<cuda_pso.gBest.y<<"iters: "<<pso.gBest.iters<<"time: "<<cuda_time<<std::endl;
+   std::cout<<"x: "<<cuda_pso.gBest.x<<"y: "<<cuda_pso.gBest.y<<"iters: "<<cuda_pso.iters<<"time: "<<cuda_time<<std::endl;
 }
