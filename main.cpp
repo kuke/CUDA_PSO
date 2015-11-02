c3d855a3b5b8f043d309d9318392c171dccbaa2a
littlekuke <littlekuke@littlekukes-MacBook-Pro.local>
2015-11-02 09:07:51 +0800

    time output

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include "pso.hpp"
 #include "cuda_pso.cuh"
+#include <string>
 
 int main()
 {
    int numParticles = 1024;
    int maxIters = 50;
+   float eps = 10^-6;
+   //static std::ofstream fout(filename,std::ios::app);
    PSO pso(numParticles);
-   float time = pso.Solve(maxIters, 10e-6);
+   float time = pso.Solve(maxIters, eps);
    std::cout<<"CPU result: "<<std::endl;
    std::cout<<"a: "<<pso.gBest.x<<" b: "<<pso.gBest.y<<" iters: "<<pso.iters<<" time: "<<time<<"ms"<<std::endl;
    
    CudaPSO cuda_pso(numParticles);
-   float cuda_time = cuda_pso.Solve(maxIters, 10^-6);
+   float cuda_time = cuda_pso.Solve(maxIters, eps);
    std::cout<<"GPU result: "<<std::endl;
    std::cout<<" a: "<<cuda_pso.gBest.x<<" b: "<<cuda_pso.gBest.y<<" iters: "<<cuda_pso.iters<<" time: "<<cuda_time<<"ms"<<std::endl;
    
    std::cout<<"GPU perf./CPU perf. = "<<time/cuda_time<<std::endl;
+   
+   std::cout<<string(system("time"))<<std::endl;
    return 0;
 }
