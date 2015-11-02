c6e757972b92ae7bc15497355d12a23a3d8abe7d
littlekuke <littlekuke@littlekukes-MacBook-Pro.local>
2015-11-02 08:45:15 +0800

    debugging

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 
 int main()
 {
-   PSO pso(1024);
-   float time = pso.Solve(50, 10e-6);
+   int numParticles = 1024;
+   int maxIters = 50;
+   PSO pso(numParticles);
+   float time = pso.Solve(maxIters, 10e-6);
    std::cout<<"CPU result: "<<std::endl;
-   std::cout<<"x: "<<pso.gBest.x<<" y: "<<pso.gBest.y<<" iters: "<<pso.iters<<" time: "<<time<<"ms"<<std::endl;
+   std::cout<<"a: "<<pso.gBest.x<<" b: "<<pso.gBest.y<<" iters: "<<pso.iters<<" time: "<<time<<"ms"<<std::endl;
    
-   CudaPSO cuda_pso(1024);
-   double start = cpu_time();
-   float cuda_time = cuda_pso.Solve(50, 10^-6);
-   cudaDeviceSynchronize();
-   double end = cpu_time();
+   CudaPSO cuda_pso(numParticles);
+   float cuda_time = cuda_pso.Solve(maxIters, 10^-6);
    std::cout<<"GPU result: "<<std::endl;
-   std::cout<<" x: "<<cuda_pso.gBest.x<<" y: "<<cuda_pso.gBest.y<<" iters: "<<cuda_pso.iters<<" time: "<<cuda_time<<"ms"<<" cpu time "<<end-start<<std::endl;
+   std::cout<<" a: "<<cuda_pso.gBest.x<<" b: "<<cuda_pso.gBest.y<<" iters: "<<cuda_pso.iters<<" time: "<<cuda_time<<"ms"<<std::endl;
+   
+   std::cout<<"GPU perf./CPU perf. = "<<time/cuda_time<<std::endl;
+   return 0;
 }
