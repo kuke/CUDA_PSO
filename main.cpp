caed8371e4649b94bc456de8f2226095dcc5072b
littlekuke <littlekuke@littlekukes-MacBook-Pro.local>
2015-11-02 08:35:00 +0800

    add sync

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
    CudaPSO cuda_pso(1024);
    double start = cpu_time();
    float cuda_time = cuda_pso.Solve(50, 10^-6);
+   cudaDeviceSynchronize();
    double end = cpu_time();
    std::cout<<"GPU result: "<<std::endl;
    std::cout<<" x: "<<cuda_pso.gBest.x<<" y: "<<cuda_pso.gBest.y<<" iters: "<<cuda_pso.iters<<" time: "<<cuda_time<<"ms"<<" cpu time "<<end-start<<std::endl;
